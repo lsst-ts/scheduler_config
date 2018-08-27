b8e3e86c8fc7cb802be7524060a602e87e955af9
Tiago Ribeiro <tiago.astro@gmail.com>
2018-08-27 08:47:38 -0700

    Testing cold start.

---
+++ config_run/sched_driver.py
@@ -0,0 +1,7 @@
+import lsst.ts.schedulerConfig.scheduler_driver
+assert type(config)==lsst.ts.schedulerConfig.scheduler_driver.SchedulerDriver, \
+    'config is of type %s.%s instead of lsst.ts.schedulerConfig.scheduler_driver.SchedulerDriver' % (
+        type(config).__module__, type(config).__name__)
+
+config.startup_type = 'HOT'
+config.startup_database = '/home/opsim/run_local/output/sextans_3663.db'
